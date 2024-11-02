6ff954b36dc7c5c458c183e81aa4463121e0d2ad
zlj0402 <2092416525@qq.com>
2024-11-02 13:47:27 +0800

    Revert "rm this LinkedList.h and re-add"
    This reverts commit 0c36c46cbd03512e777bebac92becdc019c07946.

---
+++ Include/Leetcode/LinkedList/LinkedList.h
@@ -0,0 +1,53 @@
+#ifndef LEETCODE_LINKEDLIST_H
+#define LEETCODE_LINKEDLIST_H
+
+#include <iostream>
+#include <ostream>
+
+namespace Leetcode {
+    namespace LinkedList {
+        // Definition for singly-linked list.
+        struct ListNode {
+            int val;
+            ListNode* next;
+            ListNode() : val(0), next(nullptr) {}
+            ListNode(int x) : val(x), next(nullptr) {}
+            ListNode(int x, ListNode* next) : val(x), next(next) {}
+
+            ListNode(int* arr, size_t length) {
+                ListNode* dummy_head = new ListNode(-1, nullptr);
+                ListNode* cur = dummy_head;
+                for (size_t idx = 0; idx < length; idx++) {
+                    cur->next = new ListNode(arr[idx]);
+                    cur = cur->next;
+                }
+                this->val = dummy_head->next->val;
+                this->next = dummy_head->next->next;
+            }
+
+            static void print_list(ListNode* head) {
+                ListNode* cur = head;
+                while (cur != nullptr) {
+                    cout << cur->val << "->";
+                    cur = cur->next;
+                }
+                cout << "null" << endl;
+            }
+
+            // 重载输出流操作符
+            friend ostream& operator<<(ostream& os, const ListNode* head) {
+                const ListNode* cur = head;
+                while (cur != nullptr) {
+                    os << cur->val;
+                    if (cur->next != nullptr) {
+                        os << "->";
+                    }
+                    cur = cur->next;
+                }
+                return os;
+            }
+        };
+    }
+}
+
+#endif // LEETCODE_LINKEDLIST_H
