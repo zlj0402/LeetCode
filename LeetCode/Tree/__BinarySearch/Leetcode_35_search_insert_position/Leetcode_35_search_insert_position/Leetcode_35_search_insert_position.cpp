f3b5096c20a33334b86657b365c79c8dbf3a01b0
zlj0402 <2092416525@qq.com>
2025-06-15 00:35:13 +0800

    add floort thought verietas

--- LeetCode/Tree/__BinarySearch/Leetcode_35_search_insert_position/Leetcode_35_search_insert_position/Leetcode_35_search_insert_position.cpp
+++ LeetCode/Tree/__BinarySearch/Leetcode_35_search_insert_position/Leetcode_35_search_insert_position/Leetcode_35_search_insert_position.cpp
@@ -8,7 +8,23 @@
 // Label: Binary Search
 // 
 // description:
+//  【1】.
 //      + 虽然简单，但边界不好琢磨啊
+//      + >>> 没找到的时候，不管是 l 还是 r，都会往 target 方向靠近一步，就是合适插入的地方；<<<
+//      + 这种方式是调试出来的；
+// 
+//  rank: great
+//      + 时间效率：0 ms, 击败 100%
+//      + 空间效率：13.46 MB, 击败 36.63%
+// 
+//  【2】. floor 思想改进：
+//      + floor 本身是，找到的话，返回找到的位置；
+//                     没有找到的话，返回的是前一个位置（前一个 < target 的元素的位置；升序情况的话）
+//      + 我们只要在没找到时，返回 floor 原本结果 +1 就行了；
+// 
+//  rank: great
+//      + 时间效率：0 ms, 击败 100%
+//      + 空间效率：13.38 MB, 击败 58.61%
 //
 
 #include <vector>
@@ -17,6 +33,32 @@ using std::cout;
 using std::endl;
 using std::vector;
 
+// 【2】
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+
+        int l = 0, r = nums.size() - 1;
+        int res = -1;
+        while (l <= r) {
+
+            int mid = l + (r - l) / 2;
+            if (nums[mid] <= target) {
+
+                res = mid;
+                l = mid + 1;
+            }
+            else {
+                r = mid - 1;
+            }
+        }
+
+        return res;
+    }
+};
+
+// 【1】
+/*
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
@@ -42,13 +84,15 @@ public:
             }
         }
 
+        // 没找到，那么 l 当前的位置就是适当插入的位置
         return res == -1 ? l : res;
     }
-};
+};*/
+
 
 int main() {
 
-    int target = 7;
+    int target = -1;
     vector<int> nums = { 1,3,5,6 };
     cout << (new Solution())->searchInsert(nums, target);
 }
