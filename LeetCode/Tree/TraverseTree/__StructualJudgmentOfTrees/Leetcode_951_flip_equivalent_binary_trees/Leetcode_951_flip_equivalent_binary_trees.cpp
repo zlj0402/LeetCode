ace50f395fa7535e93d689d697dd5c857cd80d16
zlj0402 <2092416525@qq.com>
2025-11-17 21:04:31 +0800

    Leetcode_951_flip_equivalent_binary_trees -- C++/Python -- Medium -- dfs

--- LeetCode/Tree/TraverseTree/__StructualJudgmentOfTrees/Leetcode_951_flip_equivalent_binary_trees/Leetcode_951_flip_equivalent_binary_trees.cpp
+++ LeetCode/Tree/TraverseTree/__StructualJudgmentOfTrees/Leetcode_951_flip_equivalent_binary_trees/Leetcode_951_flip_equivalent_binary_trees.cpp
@@ -8,7 +8,7 @@
  * 
  * @Label: Tree(dfs)
  * 
- * @Retrospect(worthy 1 - 5): 3
+ * @Retrospect(worthy 1 - 5): 4
  * 
  * @thoughts:
  *  + 【思路 1】：dfs, 这是肯定，如何具体分如何 dfs，之后来做；
@@ -18,6 +18,20 @@
  *      我无法优雅的做出来，给我考虑半天；
  * 
  *      chat 写的这个是很优雅，但还无法提前终止；
+ *      + 分析：
+ *          + 时间复杂度：O(n)
+ *          + 空间复杂度：O(h)
+ *      + rank:
+ *          + 时间效率：0 ms, 击败 100%
+ *          + 空间效率：14.98 MB, 击败 94.37%
+ * 
+ *      + 【写法 2】：ret + wrapper
+ *      + 分析：
+ *          + 时间复杂度：最坏 O(n)
+ *          + 空间复杂度：最坏 O(h)
+ *      + rank:
+ *          + 时间效率：0 ms, 击败 100%
+ *          + 空间效率：15.01 MB, 击败 88.74%
  */
 #include "../../../../../Include/Leetcode/Tree/Tree.h"
 using Leetcode::Tree::BinaryTree::TreeNode;
@@ -33,6 +47,39 @@ using Leetcode::Tree::BinaryTree::TreeNode;
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// 【思路 1】：写法 2
+class Solution {
+private:
+    bool ret;
+    bool __flipEquiv(TreeNode* root1, TreeNode* root2) {
+
+        if (!ret) return ret;
+        if (!root1 || !root2) return root1 == root2;
+        if (root1->val != root2->val) return false;
+
+
+        bool same = flipEquiv(root1->left, root2->left) &&
+                    flipEquiv(root1->right, root2->right);
+
+        bool flip = flipEquiv(root1->left, root2->right) &&
+                    flipEquiv(root1->right, root2->left);
+
+        if (ret && !(same || flip))
+            ret = false;
+
+        return same || flip;
+    }
+
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        
+        ret = true;
+        return __flipEquiv(root1, root2);
+    }
+};
+
+// 【思路 1】：写法 1
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
