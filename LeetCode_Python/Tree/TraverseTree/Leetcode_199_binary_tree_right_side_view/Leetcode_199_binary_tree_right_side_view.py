d3a76198d5ed95a0cfd68d364997bb34c9729ce0
zlj0402 <2092416525@qq.com>
2026-01-03 22:58:50 +0800

    Leetcode_199_binary_tree_right_side_view -- C++/Python -- Medium -- add BFS answer

--- LeetCode_Python/Tree/TraverseTree/Leetcode_199_binary_tree_right_side_view/Leetcode_199_binary_tree_right_side_view.py
+++ LeetCode_Python/Tree/TraverseTree/Leetcode_199_binary_tree_right_side_view/Leetcode_199_binary_tree_right_side_view.py
@@ -4,6 +4,9 @@
 @author: liangj.zhang
 @date: 14/11/2025
 
+@updated: 
+    + 3/1/2026: add 【思路 2】：BFS
+
 @Difficulty: Medium
 
 @Label: Binary Tree
@@ -17,12 +20,24 @@
     （在向下的过程中，就将深度传递开来； 不采用“回”的时候，将高度返回回来）
     
     + 分析：
-        + 时间复杂度：O(n)
-        + 空间复杂度：O(h)，h 为树的高度
+         + 时间复杂度：O(n)
+         + 空间复杂度：O(h)，h 为树的高度
 
-+ rank: 
-    时间效率：0 ms, 击败 100%
-    空间效率：17.49 MB, 击败 55.7%
+    + rank: 
+         + 时间效率：0 ms, 击败 100%
+         + 空间效率：17.49 MB, 击败 55.7%
+
+    + 【思路2】：BFS
+         BFS 层序遍历，先遍历右边节点，放入子节点时也先放入右节点；
+         内循环遍历的第一个就是右边看到的该层的第一个节点；
+         也可以正常的先遍历左节点，放入时也先放入左节点；内循环遍历的最后一个节点，就是该层从右边的第一个节点；
+        
+         + 分析：
+            + 时间复杂度：O(n)
+            + 空间复杂度：O(n)
+         + rank:
+            + 时间效率：0 ms, 击败 100%
+            + 空间效率：17.06 MB, 击败 94.28%
 """
 
 import os
@@ -42,6 +57,28 @@ from Include.PyLeetcode.Tree.Tree import TreeNode
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# 【思路 2】：BFS
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root: return []
+        ret = []
+        arr = [root]
+        tmp = []
+
+        while arr.__len__():
+            size = arr.__len__()
+            for i in range(size):
+                cur = arr[i]
+                if i == 0: ret.append(cur.val)
+                if cur.right: tmp.append(cur.right)
+                if cur.left: tmp.append(cur.left)
+            arr = tmp
+            tmp = []
+
+        return ret
+
+# 【思路 1】：DFS
 class Solution:
     def __init__(self):
         self.__max_depth = 0
