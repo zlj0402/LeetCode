cd4df992631a9413278ac2f3adf5a880fd512626
zlj0402 <2092416525@qq.com>
2026-03-08 22:10:05 +0800

    Leetcode_93 backup

--- LeetCode/BackTracking/Leetcode_93_restore_ip_addresses/Leetcode_93_restore_ip_addresses.cpp
+++ LeetCode/BackTracking/Leetcode_93_restore_ip_addresses/Leetcode_93_restore_ip_addresses.cpp
@@ -39,9 +39,88 @@ using std::vector;
 using std::string;
 using std::ostringstream;
 
+// class Solution {
+// private:
+//     static vector<string> path, ans;
+//     static bool isValid(string& sub_s) {
+
+//         if (sub_s.size() == 1) {
+//             return true;
+//         }
+//         else if (sub_s.size() <= 3 && sub_s.size() > 1) {   // 没有加位数限制，stoi 处理到 10位数时，超过 int 承受范围，会报 terminate called after throwing an instance of 'std::out_of_range'
+//             if (sub_s[0] != '0' && stoi(sub_s) <= 255) 
+//                 return true;
+//         }
+//         return false;
+//     }
+
+//     string join(const vector<string>& lst, const string& sep)
+//     {
+//         ostringstream oss;
+
+//         for (int i = 0; i < lst.size(); i++)
+//         {
+//             if (i > 0)
+//                 oss << sep;
+
+//             oss << lst[i];
+//         }
+
+//         return oss.str();
+//     }
+
+//     // remaining == 3，表示只能添加最多 3 个 '.'
+//     // size_s 保存 path 中所有字符数 -> 方便剪枝
+//     void dfs(string& s, int idx, int remaining, int size_s) {
+
+//         if (remaining == 0) {
+            
+//             if (string sub_s = s.substr(idx) ; isValid(sub_s)) {
+
+//                 path.push_back(sub_s);
+//                 ans.push_back(std::move(join(path, ".")));
+//                 path.pop_back();
+//             }
+//             return;
+//         }
+
+//         for (int j = idx; j < s.size(); ++j) {
+
+//             string sub_s = s.substr(idx, j - idx + 1);
+
+//             // 剪枝的两个关键步骤
+//             if (sub_s.size() > 3 || stoi(sub_s) > 255) return;
+//             if (s.size() - size_s - sub_s.size() > remaining * 3) continue; // 类型提升经典坑：s.size() - size_s - sub_s.size() - remaining * 3 > 0 => size_t 是 unsigned long long, 最后一步运算，int 会被提升为 unsigned long long，
+//                                                                             // s.size() = 11, size_s = 0, sub_s.size() = 3, remaining = 3，结果是远大于 0 的数 =》 最后一步 - remaining * 3 => - 9，这个 9 原来是 int，会被提升为 ULL，因为 ULL 类型更大，8(ULL) - 9(ULL) 得负数，但按照无符号数读取
+            
+//             // 只判定当前的合不合法
+//             if (isValid(sub_s)) {
+
+//                 int size = sub_s.size();
+//                 path.push_back(std::move(sub_s));
+//                 dfs(s, j + 1, remaining - 1, size_s + size);
+//                 path.pop_back();
+//             }
+//         }
+//     }
+// public:
+//     vector<string> restoreIpAddresses(string s) {
+        
+//         path.clear(); ans.clear();
+
+//         dfs(s, 0, 3, 0);
+
+//         return ans;
+//     }
+// };
+
+// vector<string> Solution::path, Solution::ans;
+
 class Solution {
+
 private:
-    static vector<string> path, ans;
+    vector<string> path;
+    vector<string> ans;
     static bool isValid(string& sub_s) {
 
         if (sub_s.size() == 1) {
@@ -54,7 +133,7 @@ private:
         return false;
     }
 
-    string join(const vector<string>& lst, const string& sep)
+    static string join(const vector<string>& lst, const string& sep)
     {
         ostringstream oss;
 
@@ -69,52 +148,20 @@ private:
         return oss.str();
     }
 
-    // remaining == 3，表示只能添加最多 3 个 '.'
-    // size_s 保存 path 中所有字符数 -> 方便剪枝
-    void dfs(string& s, int idx, int remaining, int size_s) {
+    // dfs: remaining, leftDot
+    void dfs(int idx, int lastSelected, int leftDot) {
 
-        if (remaining == 0) {
-            
-            if (string sub_s = s.substr(idx) ; isValid(sub_s)) {
 
-                path.push_back(sub_s);
-                ans.push_back(std::move(join(path, ".")));
-                path.pop_back();
-            }
-            return;
-        }
-
-        for (int j = idx; j < s.size(); ++j) {
-
-            string sub_s = s.substr(idx, j - idx + 1);
-
-            // 剪枝的两个关键步骤
-            if (sub_s.size() > 3 || stoi(sub_s) > 255) return;
-            if (s.size() - size_s - sub_s.size() > remaining * 3) continue; // 类型提升经典坑：s.size() - size_s - sub_s.size() - remaining * 3 > 0 => size_t 是 unsigned long long, 最后一步运算，int 会被提升为 unsigned long long，
-                                                                            // s.size() = 11, size_s = 0, sub_s.size() = 3, remaining = 3，结果是远大于 0 的数 =》 最后一步 - remaining * 3 => - 9，这个 9 原来是 int，会被提升为 ULL，因为 ULL 类型更大，8(ULL) - 9(ULL) 得负数，但按照无符号数读取
-            
-            // 只判定当前的合不合法
-            if (isValid(sub_s)) {
-
-                int size = sub_s.size();
-                path.push_back(std::move(sub_s));
-                dfs(s, j + 1, remaining - 1, size_s + size);
-                path.pop_back();
-            }
-        }
+        if (s.size() - idx )
     }
 public:
     vector<string> restoreIpAddresses(string s) {
-        
-        path.clear(); ans.clear();
 
-        dfs(s, 0, 3, 0);
+        dfs(0, -1, 3, s);
 
         return ans;
     }
 };
-
-vector<string> Solution::path, Solution::ans;
 
 int main() {
 
