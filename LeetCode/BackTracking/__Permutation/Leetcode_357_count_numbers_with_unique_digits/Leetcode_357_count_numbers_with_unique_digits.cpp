f1d2547ca79200047e6995e03c8c34a709e24971
zlj0402 <2092416525@qq.com>
2026-04-12 23:02:52 +0800

    Leetcode_357_count_numbers_with_unique_digits -- backtracking & permutation -- medium

--- LeetCode/BackTracking/__Permutation/Leetcode_357_count_numbers_with_unique_digits/Leetcode_357_count_numbers_with_unique_digits.cpp
+++ LeetCode/BackTracking/__Permutation/Leetcode_357_count_numbers_with_unique_digits/Leetcode_357_count_numbers_with_unique_digits.cpp
@@ -23,6 +23,15 @@
  *      + rank:
  *          + 时间效率：0 ms, 击败 100%
  *          + 空间效率：7.63 MB, 击败 81.25%
+ * 
+ *  + 【思路 2】：回溯 & 排列
+ *      回溯的方式解决排列问题，很 easy; 就是效率不高；
+ *      + 分析：
+ *          + 时间复杂度：O(2^n)
+ *          + 空间复杂度：O(n)
+ *      + rank:
+ *          + 时间效率：1063 ms, 击败 5.04%
+ *          + 空间效率：148.41 MB, 击败 5.04%
  */
 
 #include <vector>
@@ -30,59 +39,104 @@
 #include <cassert>
 using std::vector;
 
-class Solution {
-private:
-    static constexpr auto pow = [](int n, int expo) -> int {
-        assert(base >= 0 && n >= 0);
-        int sum = 1;
-        for (int i = 0; i < expo; ++i)
-            sum *= n;
-        return sum;
-    };
+#include <unordered_set>
+using std::unordered_set;
+#define us unordered_set
 
-	// A(n,r) = n! / (n - r)! ---> [n, n - r + 1]
-	static constexpr auto A = [](int n, int r) -> int {
-		assert(n >= r);
-		int sum = 1;
-		for (int i = n; i > n - r; --i)
-			sum *= i;
-		return sum;
-	};
-	
-	// C(n,r) = n! / ( (n - r)! * r! ) = A(n,r) / r!
-	static constexpr auto C = [](int n, int r) -> int {
-		assert(n >= r);
-		int rFactorial = 1;
-		for (int i = 1; i <= r; ++i)
-			rFactorial *= i;
-		return A(n, r) / rFactorial;
-	};
-	
-	// n 位数，不存在重复数字的可能
-	int countNDigitUniqueNumbers(int n) {
-        assert(n >= 1);
-        if (n == 1) return C(10, 1);
-        return 9 * A(9, n - 1);	
-    }
-	
-	static vector<int> rec;
+class Solution {
+private: 
+static vector<int> nRec;
+
 public:
     int countNumbersWithUniqueDigits(int n) {
-      	if (n == 0) return 1;
+        if (n == 0) return 1;
+
+        us<int> rec;
+        int cnt = 0;
+        auto dfs = [&](this auto&& dfs, int idx, int n) -> void {
+            if (idx == n) {
+                ++cnt;
+                return;
+            }
+
+            for (int i = (n == 1 ? 0 : (idx == 0 ? 1 : 0)); i <= 9; ++i) {
+                if (rec.find(i) == rec.end()) {
+                    rec.insert(i);
+                    dfs(idx + 1, n);
+                    rec.erase(i);
+                }
+            }
+        };
+
         int sum = 0;
         for (int i = 1; i <= n; ++i) {
-            if (rec[i] == 0) {
-                rec[i] = countNDigitUniqueNumbers(i);
-                // std::cout << i << " " << rec[i] << std::endl;
+            if (nRec[i] == 0) {
+                cnt = 0;
+                dfs(0, i);
+                nRec[i] = cnt;
+                // std::cout << cnt << std::endl;
             }
-            sum += rec[i];
+            sum += nRec[i];
         }
         return sum;
     }
 };
 
-vector<int> Solution::rec(9, 0);
+vector<int> Solution::nRec(9, 0);
+
+// class Solution {
+// private:
+//     static constexpr auto pow = [](int n, int expo) -> int {
+//         assert(base >= 0 && n >= 0);
+//         int sum = 1;
+//         for (int i = 0; i < expo; ++i)
+//             sum *= n;
+//         return sum;
+//     };
+
+// 	// A(n,r) = n! / (n - r)! ---> [n, n - r + 1]
+// 	static constexpr auto A = [](int n, int r) -> int {
+// 		assert(n >= r);
+// 		int sum = 1;
+// 		for (int i = n; i > n - r; --i)
+// 			sum *= i;
+// 		return sum;
+// 	};
+	
+// 	// C(n,r) = n! / ( (n - r)! * r! ) = A(n,r) / r!
+// 	static constexpr auto C = [](int n, int r) -> int {
+// 		assert(n >= r);
+// 		int rFactorial = 1;
+// 		for (int i = 1; i <= r; ++i)
+// 			rFactorial *= i;
+// 		return A(n, r) / rFactorial;
+// 	};
+	
+// 	// n 位数，不存在重复数字的可能
+// 	int countNDigitUniqueNumbers(int n) {
+//         assert(n >= 1);
+//         if (n == 1) return C(10, 1);
+//         return 9 * A(9, n - 1);	
+//     }
+	
+// 	static vector<int> rec;
+// public:
+//     int countNumbersWithUniqueDigits(int n) {
+//       	if (n == 0) return 1;
+//         int sum = 0;
+//         for (int i = 1; i <= n; ++i) {
+//             if (rec[i] == 0) {
+//                 rec[i] = countNDigitUniqueNumbers(i);
+//                 // std::cout << i << " " << rec[i] << std::endl;
+//             }
+//             sum += rec[i];
+//         }
+//         return sum;
+//     }
+// };
+
+// vector<int> Solution::rec(9, 0);
 
 int main() {
-    Solution().countNumbersWithUniqueDigits(8);
+    std::cout << Solution().countNumbersWithUniqueDigits(2) << std::endl;
 }
