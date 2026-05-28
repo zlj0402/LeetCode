6594d84f9877fd2e171f5359dd07653577f353fc
unknown <zhangliangji@auxgroup.com>
2026-05-28 19:35:13 +0800

    Leetcode_2915_length_of_the_longest_subsequence_that_sums_to_target -- C++ -- DP 01-Knapsack -- sparse DP -- Medium

--- LeetCode/DynamicProgramming/__01Knapsack/Leetcode_2915_length_of_the_longest_subsequence_that_sums_to_target/Leetcode_2915_length_of_the_longest_subsequence_that_sums_to_target.cpp
+++ LeetCode/DynamicProgramming/__01Knapsack/Leetcode_2915_length_of_the_longest_subsequence_that_sums_to_target/Leetcode_2915_length_of_the_longest_subsequence_that_sums_to_target.cpp
@@ -8,6 +8,8 @@
  *  + 26/5/2026: 【思路 1/2.1/2.2】
  *  + 27/5/2026: 【思路 3 -- 写法 1】：动态规划 & 状态压缩
  *               【思路 3 -- 写法 2】：动态规划 & 状态压缩 -- 再进一步的剪枝优化
+ *  + 28/5/2026: 【思路 4 -- 写法 1】：动态规划 & 状态压缩 -- 稀疏 DP -- 类似一个数组的做法；
+ *               【思路 4 -- 写法 2】：动态规划 & 状态压缩 -- 真正的稀疏 DP -- 类似滚动数组的做法；
  * 
  * @Difficulty: Medium
  * 
@@ -66,77 +68,164 @@
  *      再细细回想，递推填表，还是 n + 1 组状态时，
  *      从 idx 1 开始，只有 t = nums[0] 的状态去更新一下就可以，后续的得到的都是默认状态；
  *         idx 2 开始，只有 t = nums[1], nums[0] + nums[1] 的状态去更新一下， t ∈ (nums[0], nums[0] + nums[1]) 之间的状态都不会有，直接继承默认就可以；
- *         idx 3 , 就是简单的枚举了，
+ *         idx 3 , 就不是简单的枚举了，在前一个结果之后，所有的状态基础都加上 nums[idx](当然要 < target)
  *      但可以将已有的状态放 map 当中，所以的情况 key(即 t)，加上 nums[i]，得到的状态 t_，如果已经存在，就跟 map[t] + 1 相比；
  *                                                                                   如果不存在，直接赋值 map[t] + 1
+ *  + 【思路 4 -- 写法 1】：动态规划 & 状态压缩 -- 稀疏 DP -- 类似一个数组的做法；
+ *      想清楚，我们其实始终，都是在前一个状态组的基础上，加上当前的 nums[i]，得到新的状态值；
+ *          前一组有效状态位置的基础上，加上 nums[i]，只要 pre + nums[i] <= target 都可以处理；
+ *          >>> 还要注意的一点是：我们需要从后往前遍历，从后面遍历，前面的状态对应的值，还没有在这一轮被改变；<<<
+ *     + 分析：
+ *          + 时间复杂度：O(n * (target/2) * log(target)) ==> O(n*target*log(target))
+ *              因为我采取的是 map，可以按值的顺序进行遍历，查找插入都是 O(log(target))
+ *              其次状态最坏，每次都产生前面完整一组的个数新状态（但也是控制在 target 之内），一组状态最多是 target+1 个；
+ *              所以上面的，target 没有错；
+ *              ---
+ *              >>> 不过在 target 非常大的时候，nums.size() 比较小的时候，还是有优势的 <<<
+ *          + 空间复杂度：O(target)
+ *      + rank: 超时 -- 一开始确实觉得插入查找可能影响比较大，
+ *                      但是，下面的写法二（类似滚动数组，这里使用 unordered_map）也是超时，我就有点怀疑了，插入查找都是 O(1)，尽然也没有过，
+ *                      而且，差的就是最后一个 case，这个 case 就两个输入值，来回跑两次，都是最后一次 case，我怀疑它就是不想让 map 的方式通过；
+ *  + 【思路 4 -- 写法 2】：动态规划 & 状态压缩 -- 真正的稀疏 DP -- 类似滚动数组的做法；
+ *      也是基于前一组的状态，进行改变，
+ *      这里使用拷贝一个 map，作为原来的状态组，有新的状态，将其和值，更新到"全局"的当中；
+ *      + 分析：
+ *          + 时间复杂度：O(n * target)
+ *              每次拷贝 O(S_i)，总的：O(S_1 + S_2 + ... + S_n)，每一个值是一组状态，最坏一组 target 个，
+ *              最坏是 O(n * target)
+ *          + 空间复杂度：O(2 * target) == O(target)
+ *      + rank: 超时 -- 不可思议，被做局了；
  */
 
 #include <vector>
 #include <iostream>
 using std::vector;
 
-// 【思路 3 -- 写法 2】：动态规划 & 状态压缩 -- 再进一步的剪枝优化
+// 【思路 4 -- 写法 2】：动态规划 & 状态压缩 -- 真正的稀疏 DP -- 类似滚动数组的做法；
+// chat 给的稀疏 DP
+class Solution {
+public:
+    int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+
+        unordered_map<int, int> dp;
+        dp[0] = 0;
+
+        for (int num : nums) {
+            auto old = dp;
+            for (auto &[sum, len] : old) {
+                int ns = sum + num;
+                if (ns > target) continue;
+                dp[ns] = std::max(dp[ns], len + 1);
+            }
+        }
+        return dp.count(target) ? dp[target] : -1;
+    }
+};
+
+// 【思路 4 -- 写法 1】：动态规划 & 状态压缩 -- 稀疏 DP -- 类似一个数组的做法；
+// 我以为的稀疏 DP
+#include <unordered_map> // 状态的更新，必须是按顺序进行的；
+using std::unordered_map; // 就像使用一个数组的压缩方式时，如果从前往后遍历是不对的（后面是需要依赖前面没有变换之前的值），需要从后往前；
+#define um unordered_map
+#include <map>
+using std::map;
+// 总复杂度接近：O(n * target * log(target))
 class Solution {
 public:
     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
         int n = nums.size();
 
-        vector<int> dp(target + 1, -1);
+        map<int, int> dp;    // <status, status_val>
         dp[0] = 0;
-
-        int s = 0;
         for (auto num : nums) {
-            s += num;
-            // 第一次只会遍历 1 次，非常 nice
-            // 第二次只会遍历 [nums[i], min(target, s)]
-            // for (int t = std::min(target, s); t >= num; --t) {
-            //     if (int pre = dp[t - num]; dp[t] == -1) {
-            //         if (pre != -1) dp[t] = pre + 1;
-            //     }
-            //     else {
-            //         // if (pre != -1) => 这里加或不加，都不影响了，理论上是可以加的，表示前面对应位置是有效的；
-            //         dp[t] = std::max(dp[t], pre + 1);
-            //     }
+            // for (auto &[k, v] : dp) {
+            //     // 想象压缩成一个数组的过程，
+            //     // [nums[i], target]，是依赖前一次集合的状态值，< nums[i] 直接继承原来的
+
+            //     // 我们现在也从大到小遍历，考量 k + nums[i] <= target 时，
             // }
-
-            // 进而可以简洁成下面这样：
-            for (int t = std::min(target, s); t >= num; --t) {
-                if (int pre = dp[t - num]; pre != -1) {
-                    dp[t] = std::max(dp[t], pre + 1);
-                }
-            }
-        }
-        return dp[target];
-    }
-};
-
-// 【思路 3 -- 写法 1】：动态规划 & 状态压缩
-class Solution {
-public:
-    int lengthOfLongestSubsequence(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        vector<int> dp(target + 1, -1);
-        dp[0] = 0;
-
-        for (int i = 0; i < n; ++i) {
-            for (int t = target; t >= nums[i]; --t) {
-                if (int pre = dp[t - nums[i]]; dp[t] == -1) {
-                    if (pre != -1) dp[t] = pre + 1;
-                }
+            for (auto it = dp.rbegin(); it != dp.rend(); ++it) {
+                if (int cur = it->first + num; cur > target) continue;
                 else {
-                    dp[t] = std::max(dp[t], pre + 1);
+                    // if (dp.find(cur) != dp.end()) {
+                    //     dp[cur] = std::max(dp[cur], dp[it->first] + 1);
+                    // }
+                    // else {
+                    //     dp[cur] = dp[it->first] + 1;
+                    // }
+                    // 可以简化为：因为 dp[cur] 不存在是，得到的值是 0
+                    dp[cur] = std::max(dp[cur], dp[it->first] + 1);
                 }
-            }
+            } 
         }
-
-        return dp[target];
+        return dp.count(target) ? dp[target] : -1;
     }
 };
+
+// // 【思路 3 -- 写法 2】：动态规划 & 状态压缩 -- 再进一步的剪枝优化
+// class Solution {
+// public:
+//     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+//         int n = nums.size();
+
+//         vector<int> dp(target + 1, -1);
+//         dp[0] = 0;
+
+//         int s = 0;
+//         for (auto num : nums) {
+//             s += num;
+//             // 第一次只会遍历 1 次，非常 nice
+//             // 第二次只会遍历 [nums[i], min(target, s)]
+//             // for (int t = std::min(target, s); t >= num; --t) {
+//             //     if (int pre = dp[t - num]; dp[t] == -1) {
+//             //         if (pre != -1) dp[t] = pre + 1;
+//             //     }
+//             //     else {
+//             //         // if (pre != -1) => 这里加或不加，都不影响了，理论上是可以加的，表示前面对应位置是有效的；
+//             //         dp[t] = std::max(dp[t], pre + 1);
+//             //     }
+//             // }
+
+//             // 进而可以简洁成下面这样：
+//             for (int t = std::min(target, s); t >= num; --t) {
+//                 if (int pre = dp[t - num]; pre != -1) {
+//                     dp[t] = std::max(dp[t], pre + 1);
+//                 }
+//             }
+//         }
+//         return dp[target];
+//     }
+// };
+
+// // 【思路 3 -- 写法 1】：动态规划 & 状态压缩
+// class Solution {
+// public:
+//     int lengthOfLongestSubsequence(vector<int>& nums, int target) {
+//         int n = nums.size();
+
+//         vector<int> dp(target + 1, -1);
+//         dp[0] = 0;
+
+//         for (int i = 0; i < n; ++i) {
+//             for (int t = target; t >= nums[i]; --t) {
+//                 if (int pre = dp[t - nums[i]]; dp[t] == -1) {
+//                     if (pre != -1) dp[t] = pre + 1;
+//                 }
+//                 else {
+//                     dp[t] = std::max(dp[t], pre + 1);
+//                 }
+//             }
+//         }
+
+//         return dp[target];
+//     }
+// };
 
 int main() {
-    vector<int> nums = {1, 2, 3, 4, 5};
-    int target = 9;
+    // vector<int> nums = {1, 2, 3, 4, 5};//4,1,3,2,1,5
+    vector<int> nums = {4,1,3,2,1,5};
+    // int target = 9;
+    int target = 7;
     std::cout << Solution().lengthOfLongestSubsequence(nums, target) << std::endl;
 }
 
