667acd5115133ce4be338361d008fe8c30e0eb7d
unknown <zhangliangji@auxgroup.com>
2026-09-20 10:09:58 +0800

    Leetcode_64_最小路径和_2nd -- C++ -- DP -- Medium

--- LeetCode/DynamicProgramming/Leetcode_64_minimum_path_sum/Leetcode_64_minimum_path_sum_2nd.cpp
+++ LeetCode/DynamicProgramming/Leetcode_64_minimum_path_sum/Leetcode_64_minimum_path_sum_2nd.cpp
@@ -4,6 +4,9 @@
  * @author: liangj.zhang
  * @date: 2026/09/19
  * 
+ * @updated:
+ *  + 2026/09/20: add 【思路 2 -- 写法 1】：记忆化搜索 -- 先上再左，【思路 2 -- 写法 2】：记忆化搜索 -- 先左再上
+ * 
  * @Difficulty: Medium
  * 
  * @Label: DP
@@ -30,27 +33,95 @@
  * 
  * ===
  *  不知道这题递归能不能解，但肯定是没有上面这个方式简单；
+ * 
+ *  + 【思路 2 -- 写法 1】：记忆化搜索 -- 先上再左，
+ *      递归能解，从最右下方，递到最左上方；归的时候，是完全相反的过程；直到一个没有递过的分支，再到底；（图纸上画画就能理解）
+ *      + 分析：
+ *          + 时间复杂度：O(mn) 状态数
+ *          + 空间复杂度：O(mn)备份的rec + O(m + n)递归 == O(mn)
+ *      + rank:
+ *          + 时间效率：0 ms, 击败 100%
+ *          + 空间效率：15.98 MB, 击败 52.58%
+ * 
+ *  + 【思路 2 -- 写法 2】：记忆化搜索 -- 先左再上
+ *      同 写法1 差不多
+ *      + 分析：同上
+ *      + rank:
+ *          + 时间效率：3 ms, 击败 29.31%
+ *          + 空间效率：16.07 MB, 击败 33.84%
  */
 
 #include <vector>
+#include <iostream>
 using std::vector;
 
+// 【思路 2 -- 写法 1】：记忆化搜索 -- 先上再左，
 class Solution {
 private:
-    static constexpr int MAXVAL = 80001; // 原来错误的写法： MAXVAL = 201; 错误，因为前两个位置的值也是叠加上来的
+    static constexpr int MAXVAL = 80000;
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m = grid[0].size();
-        int n = grid.size();
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                int val1 = MAXVAL, val2 = MAXVAL;
-                if (i - 1 >= 0) val1 = grid[i - 1][j]; // 上一行的位置
-                if (j - 1 >= 0) val2 = grid[i][j - 1]; // 上一列的位置
-                int min_ = std::min(val1, val2);
-                grid[i][j] += min_ == MAXVAL ? 0 : min_;
-            }
-        }
-        return grid[n-1][m-1];
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> rec(m, vector<int>(n, -1));
+
+        auto dfs = [&](this auto&& dfs, int i, int j) {
+            if (i < 0 || j < 0) return MAXVAL;
+            if (rec[i][j] != -1) return rec[i][j];
+
+            int min_ = std::min(dfs(i - 1, j), dfs(i, j - 1));
+
+            return rec[i][j] = grid[i][j] + ((min_ == MAXVAL) ? 0 : min_);
+        } (m - 1, n - 1);
+
+        return rec[m - 1][n - 1];
     }
 };
+
+// 【思路 2 -- 写法 2】：记忆化搜索 -- 先左再上
+class Solution {
+private:
+    static constexpr int MAXVAL = 80000;
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> rec(m, vector<int>(n, -1));
+
+        auto dfs = [&](this auto&& dfs, int i, int j) {
+            if (i < 0 || j < 0) return MAXVAL;
+            if (rec[i][j] != -1) return rec[i][j];
+
+            int min_ = std::min(dfs(i, j - 1), dfs(i - 1, j));
+
+            return rec[i][j] = grid[i][j] + ((min_ == MAXVAL) ? 0 : min_);
+        } (m - 1, n - 1);
+
+        return rec[m - 1][n - 1];
+    }
+};
+
+// class Solution {
+// private:
+//     static constexpr int MAXVAL = 80000; // 原来错误的写法： MAXVAL = 201; 错误，因为前两个位置的值也是叠加上来的
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int m = grid[0].size();
+//         int n = grid.size();
+//         for (int i = 0; i < n; ++i) {
+//             for (int j = 0; j < m; ++j) {
+//                 int val1 = MAXVAL, val2 = MAXVAL;
+//                 if (i - 1 >= 0) val1 = grid[i - 1][j]; // 上一行的位置
+//                 if (j - 1 >= 0) val2 = grid[i][j - 1]; // 上一列的位置
+//                 int min_ = std::min(val1, val2);
+//                 grid[i][j] += min_ == MAXVAL ? 0 : min_;
+//             }
+//         }
+//         return grid[n-1][m-1];
+//     }
+// };
+
+int main() {
+    vector<vector<int>> grid = {{1,3,1}, {1,5,1}, {4,2,1}};
+    std::cout << Solution().minPathSum(grid) << std::endl;
+}
