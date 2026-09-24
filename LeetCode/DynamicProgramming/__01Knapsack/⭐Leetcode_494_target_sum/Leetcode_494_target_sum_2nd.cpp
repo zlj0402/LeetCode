f53f7ee1d5efa7e0f11e01f4cc0be6ab1a02a225
unknown <zhangliangji@auxgroup.com>
2026-09-24 09:39:31 +0800

    Leetcode_494_目标和_2nd -- C++ -- 01Knapsack -- Medium

--- LeetCode/DynamicProgramming/__01Knapsack/⭐Leetcode_494_target_sum/Leetcode_494_target_sum_2nd.cpp
+++ LeetCode/DynamicProgramming/__01Knapsack/⭐Leetcode_494_target_sum/Leetcode_494_target_sum_2nd.cpp
@@ -6,6 +6,7 @@
  * 
  * @updated:
  *      + 2026/09/23: add【思路 2】：记忆化搜索
+ *      + 2026/09/24: add【思路 3】：动态规划 -- 状态压缩
  * 
  * @Difficulty: Medium
  * 
@@ -43,12 +44,50 @@
  *              + t 取 (sum - |target|) / 2 而非 (sum + target) / 2 是为了让背包容量更小，优化常数
  *          当 sum 较大但 n 较小时，这个 DP 可能不如直接 DFS（指数级但实际剪枝多）；
  *          当 sum 适中时，这个 DP 是伪多项式时间的高效解法。
+ *  + 【思路 3】：动态规划 -- 状态压缩（直接跳过了滚动数组了..）
+ *      + 分析: 
+ *          + 时间复杂度：O(n * t) == O(n * sum)
+ *          + 空间复杂度：O(t) == O(sum)
+ *      + rank:
+ *          + 时间效率：0 ms, 击败 100%
+ *          + 空间效率：12.31 MB, 击败 53.26%
  */ 
 #include <vector>
 #include <numeric>
 #include <cmath>
 using std::vector;
 
+// 【思路 3】：动态规划 -- 状态压缩
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        // 现在问题已经变成，从 nums 当中找出和为 (sum - abs(target)) / 2 的可能
+        int sum = std::accumulate(nums.begin(), nums.end(), 0);
+        int t = (sum - abs(target)) / 2;
+        if (t < 0 || (sum - abs(target)) % 2) return 0;
+
+        int n = nums.size();
+        vector<int> rec = vector<int>(t + 1, 0); // 现在存的是 t+1 范围内，每个位置的可能性
+        rec[0] = 1; // 每个 idx，都有一个范围段，是前面 [0, idx-1] 
+
+        int sum_ = 0;
+        for(int i = 0; i < n; ++i) {
+            sum_ += nums[i];
+            for (int j = std::min(t, sum_); j >= nums[i]; --j) {
+                // 每个位置的可能性 = 上次该位置遗留下来的 + （该位置 - nums[i] 上一次遗留值）
+                /**
+                 * 参考 【思路 2】 的：
+                 * if (left < nums[idx]) return r = dfs(idx - 1, left); =》 当值 j < nums[i]，那么直接用继承过来的值就可以了；
+                 * return r = dfs(idx - 1, left - nums[idx]) + dfs(idx - 1, left); =》 要么再多一种可能，是从 rec[j - nums[i]] 跳过来的，两个位置的可能性相加
+                 */
+                rec[j] += rec[j - nums[i]];
+            }
+        }
+        return rec[t];
+    }
+};
+
+//【思路 2】：记忆化搜索
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
@@ -83,6 +122,7 @@ public:
     }
 };
 
+// 【思路 1】：纯 dfs
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
